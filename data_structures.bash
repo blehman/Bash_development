63884ac200201e59ae0d448c55e72a499393478e
Brian Lehman <pedalproject.com@gmail.com>
2014-01-10 00:43:58 -0700

    added dates

--- data_structures.bash
+++ data_structures.bash
@@ -46,4 +46,27 @@ for i in "${!twitter_handle[@]}"; do
     echo "value: ${twitter_handle[$i]}"
 done
 
+################
+# date object
+################
+
+#--create range
+start_date="2014-01-09 22:00:00";  
+end_date="2014-01-10 22:00:00";  
+
+#--create date objects
+current=$(date -d "${start_date:0:4}${start_date:5:2}${start_date:8:2} ${start_date:11:2}")
+end=$(date -d "${end_date:0:4}${end_date:5:2}${end_date:8:2} ${end_date:11:2}")
+
+#--loop
+while [ "$end" != "$current" ];do
+    path=$(date -d "$current" +%Y%m%d.%H)
+    year="${path:0:4}"
+    mnth="${path:4:2}"
+    day="${path:6:2}"
+    hour="${path:9:2}"
+    echo $path
+    current=$(date -d "$current +1 hours")
+done
+
 
